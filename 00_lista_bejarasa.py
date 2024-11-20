3cbbca1dba30d7cba7fb75cd9db6039f71810589
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2024-11-20 13:24:32 +0100

    Update 00_lista_bejarasa.py

--- 00_lista_bejarasa.py
+++ 00_lista_bejarasa.py
@@ -1,16 +1,27 @@
 # gyümölcsök kiíratása
 gyumolcsok = ['alma', 'körte', 'szilva', 'barack']
 
-print()
+print(gyumolcsok)
+
+for gyumolcs in gyumolcsok:
+    print(gyumolcs)
 
 # hónapok kiíratása
 honapok = ['január', 'február','március', 'április', 'május', 'június', 'július'] 
 
-print()
+for honap in honapok:
+    print(honap)
 
 # index megjelenítése:
     # 0 január
     # 1 február
+
+
+
+honap_index = 1
+for honap in honapok:
+    print(f"{honap_index} {honap}")
+    honap_index += 1
 
 print()
 
