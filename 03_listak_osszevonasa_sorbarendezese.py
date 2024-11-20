131967ac874625f0be56b5385b030e4c11084e42
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2024-11-20 14:24:19 +0100

    add list

--- 03_listak_osszevonasa_sorbarendezese.py
+++ 03_listak_osszevonasa_sorbarendezese.py
@@ -2,6 +2,16 @@
 Hozz létre két listát: [3, 1, 4] és [9, 7, 2]. 
 Egyesítsd a két listát, és rendezd a lista elemeit növekvő sorrendbe!
 """
+lista1 = [3, 1, 4]
+lista2 = [9, 7, 2]
 
+lista1.extend(lista2)
+lista1.sort()
+# lista1.sort(reverse=False)
+# lista1.sort(reverse=True)
+print(lista1)
 
 # írasd ki az első és az utolsó elemet!
+
+print(lista1[0])
+print(lista1[-1])
